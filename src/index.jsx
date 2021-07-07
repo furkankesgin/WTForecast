6d1a274609b57ad4e7caf536920dddd4f857fa72
Furkan <furkan_bfk@hotmail.com>
2021-07-07 12:40:37 +0300

    iphone5 phone size added with landscape

--- src/index.jsx
+++ src/index.jsx
@@ -2,9 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import GlobalStyle from "./styles/GlobalStyle";
+import Iphone5size from "./styles/phone-sizes/Iphone5size";
+
 ReactDOM.render(
 	<React.StrictMode>
 		<GlobalStyle/>
+		<Iphone5size />
 		<App />
 	</React.StrictMode>,
 	document.getElementById('root')
