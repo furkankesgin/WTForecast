77e56c4505b614f68945d5f48e8508041e17b180
cccaaannn <can.kurt.aa@gmail.com>
2021-07-07 22:00:04 +0300

    fractionDigits option added

--- src/helpers/convertTemp.js
+++ src/helpers/convertTemp.js
@@ -1,4 +1,4 @@
-export function convertTemp(temp, tempUnit){
+export function convertTemp(temp, tempUnit, fractionDigits=1){
     // convert to Fahrenheit
     if(tempUnit === "F"){
         temp = temp * 9/5 - 459.67;
@@ -7,7 +7,7 @@ export function convertTemp(temp, tempUnit){
     else{
         temp = temp - 272.15;
     }
-    temp = temp.toLocaleString('en-US', {maximumFractionDigits:1});
+    temp = temp.toLocaleString('en-US', {maximumFractionDigits:fractionDigits});
     return temp
 }
 
