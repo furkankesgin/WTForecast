f6c5a36e24df550f821114b2d19270fc6d859266
cccaaannn <can.kurt.aa@gmail.com>
2021-07-21 01:00:15 +0300

    static URLS and UNITS added as seperate files

---
+++ src/statics/URLS.js
@@ -0,0 +1,7 @@
+const weatherAPICallBaseURL = "https://api.openweathermap.org/data/2.5/weather";
+const forecastAPICallBaseURL = "https://api.openweathermap.org/data/2.5/forecast";
+
+const iconBaseURL = "http://openweathermap.org/img/wn/";
+const placeHolderImageURL = "https://github.com/github.png";
+
+export { weatherAPICallBaseURL, forecastAPICallBaseURL, iconBaseURL, placeHolderImageURL };
