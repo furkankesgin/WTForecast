319c3ff94cbd5995856e45d82263d6f8589f7830
cccaaannn <can.kurt.aa@gmail.com>
2021-07-21 00:57:37 +0300

    settings form added to CurrentWeatherCardBack

--- src/components/card1/CurrentWeatherCardBack.jsx
+++ src/components/card1/CurrentWeatherCardBack.jsx
@@ -1,71 +1,191 @@
-import styled from 'styled-components';
+// bootstrap
+import { Card, Row, Col } from 'react-bootstrap'
 
 const CurrentWeatherCardBack = (props) => {
+
+    const toggleCustomUI = (name) => {
+        // checkbox id's used as name
+        props.setCustomUIElements({...props.customUIElements, [name]:!props.customUIElements[name]});
+    }
+
+    const changeUnit = (unit) => {
+        // checkbox id's used as unit
+        props.setUnit(unit);
+    }
+
     return (
-        // <CurrentWeatherCardBackStyle style={props.settingsVisibility ? {transform: `rotateY(360deg)`} : {transform: `rotateY(180deg)`}}>
-        <CurrentWeatherCardBackStyle>
-            <p>{props.width}</p>
-            
-            <SettingsFormStyle>
-                <label>test</label>
-                <input type='text' placeholder='test' />
-                <br /><br />
-                <label>test</label>
-                <input type='text' placeholder='test' />
-                <br /><br />
-                <label>test</label>
-                <input type='text' placeholder='test' />    
-            </SettingsFormStyle> 
-        </CurrentWeatherCardBackStyle>
+        <Card className="card-color2 p-3">
+            <Row>
+
+                {/* city selection */}
+                <Col className="col-xxl-6 col-xl-6 col-lg-12 col-md-12 col-sm-12 col-12">
+                    <p className="h4">Choose City</p>
+                    <div className="form-floating mb-3"> {/* if value isCityValid is unset show no message if it is set show error or success messages */}
+                        <input type="text" className={`form-control ${props.isCityValid}`} placeholder="City" aria-label="city-selection" aria-describedby="city-selection" 
+                        onChange={(e) => props.setCitySearch(e.target.value)}/>
+                        <label for="city-selection" className="form-label">Search City</label>
+                        
+                        <div className="valid-feedback">
+                            City Found!
+                        </div>
+                        <div className="invalid-feedback">
+                            City Not Found
+                        </div>
+                    </div>
+                </Col>
+
+                {/* Unit selection */}
+                <Col className="col-xxl-3 col-xl-3 col-lg-6 col-md-6 col-sm-0 col-0">
+                    <p className="h4">Choose Unit</p>
+                    <div className="form-check">
+                        <input className="form-check-input" type="radio" id="metric" name="unitRadio"
+                        checked={props.unit === "metric" ? "checked" : ""}
+                        onChange={(e) => changeUnit(e.target.id)} />
+
+                        <label className="form-check-label" for="metricRadio">
+                            Metric (˚C)
+                        </label>
+                    </div>
+
+                    <div className="form-check">
+                        <input className="form-check-input" type="radio" id="imperial" name="unitRadio"
+                        checked={props.unit === "imperial" ? "checked" : ""}
+                        onChange={(e) => changeUnit(e.target.id)} />
+
+                        <label className="form-check-label" for="imperialRadio">
+                            Imperial (˚F)
+                        </label>
+                    </div>
+                </Col>
+
+                {/* theme selection TODO*/}
+                <Col className="col-xxl-3 col-xl-3 col-lg-6 col-md-6 col-sm-0 col-0">
+                    <p className="h4">Choose Theme</p>
+                    <div className="form-check">
+                        <input className="form-check-input" type="radio" id="themeRadio1" name="themeRadio" checked
+                        // checked={props.unit === "metric" ? "checked" : ""}
+                        onChange={(e) => console.log("theme 1 button")} />
+
+                        <label className="form-check-label" for="themeRadio1">
+                            theme 1
+                        </label>
+                    </div>
+
+                    <div className="form-check">
+                        <input className="form-check-input" type="radio" id="themeRadio2" name="themeRadio"
+                        // checked={props.unit === "imperial" ? "checked" : ""}
+                        onChange={(e) => console.log("theme 2 button")} />
+
+                        <label className="form-check-label" for="themeRadio2">
+                            theme 2
+                        </label>
+                    </div>
+                </Col>
+
+            </Row>
+
+            {/* ui customization */}
+            <Row>
+                {/* other temp elements */}
+                <p className="h4 mt-2">Customize Look</p>
+                <Col>
+                    <Row>
+                        <Col>
+                            <div class="form-check form-switch">
+                                <input class="form-check-input" type="checkbox" id="pressure" 
+                                onChange={(e) => toggleCustomUI(e.target.id)}
+                                checked={props.customUIElements.pressure ? "checked" : ""} />
+                                <label class="form-check-label" for="pressureCheckBox">Pressure</label>
+                            </div>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col>
+                            <div class="form-check form-switch">
+                                <input class="form-check-input" type="checkbox" id="humidity" 
+                                onChange={(e) => toggleCustomUI(e.target.id)}
+                                checked={props.customUIElements.humidity ? "checked" : ""}/>
+                                <label class="form-check-label" for="humidityCheckBox">Humidity</label>
+                            </div>
+                        </Col>
+                    </Row>
+                </Col>
+                <Col>
+                    <Row>
+                        <Col>
+                            <div class="form-check form-switch">
+                                <input class="form-check-input" type="checkbox" id="wind" 
+                                onChange={(e) => toggleCustomUI(e.target.id)}
+                                checked={props.customUIElements.wind ? "checked" : ""}/>
+                                <label class="form-check-label" for="windCheckBox">Wind</label>
+                            </div>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col>
+                            <div class="form-check form-switch">
+                                <input class="form-check-input" type="checkbox" id="cloudiness" 
+                                onChange={(e) => toggleCustomUI(e.target.id)}
+                                checked={props.customUIElements.cloudiness ? "checked" : ""}/>
+                                <label class="form-check-label" for="cloudinessCheckBox">Cloudiness</label>
+                            </div>
+                        </Col>
+                    </Row>
+                </Col>
+
+                {/* min max - feels like */}
+                <Col>
+                    <Row>
+                        <Col>
+                            <div class="form-check form-switch">
+                                <input class="form-check-input" type="checkbox" id="minmax" 
+                                onChange={(e) => toggleCustomUI(e.target.id)}
+                                checked={props.customUIElements.minmax ? "checked" : ""}/>
+                                <label class="form-check-label" for="minmaxCheckBox">Min Max</label>
+                            </div>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col>
+                            <div class="form-check form-switch">
+                                <input class="form-check-input" type="checkbox" id="feels" 
+                                onChange={(e) => toggleCustomUI(e.target.id)}
+                                checked={props.customUIElements.feels ? "checked" : ""}/>
+                                <label class="form-check-label" for="feelsCheckBox">Feels like</label>
+                            </div>
+                        </Col>
+                    </Row>
+                </Col>
+
+                {/* lon lat - sun */}
+                <Col>
+                    <Row>
+                        <Col>
+                            <div class="form-check form-switch">
+                                <input class="form-check-input" type="checkbox" id="lonlat" 
+                                onChange={(e) => toggleCustomUI(e.target.id)}
+                                checked={props.customUIElements.lonlat ? "checked" : ""}/>
+                                <label class="form-check-label" for="lonlatCheckBox">Lon Lat</label>
+                            </div>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col>
+                            <div class="form-check form-switch">
+                                <input class="form-check-input" type="checkbox" id="sun" 
+                                onChange={(e) => toggleCustomUI(e.target.id)}
+                                checked={props.customUIElements.sun ? "checked" : ""}/>
+                                <label class="form-check-label" for="sunCheckBox">Sunrise/set</label>
+                            </div>
+                        </Col>
+                    </Row>
+                </Col>
+
+            </Row>
+
+        </Card>
     )
 }
-
-const CurrentWeatherCardBackStyle = styled.div`
-    /* position: absolute; */
-    width: 90%; 
-    /* height: fit-content; */
-
-    transition: transform 1s;
-    backface-visibility:hidden;
-    background-color: #17B978;
-
-    border-radius: 15px;
-    /* padding: 0.7rem; */
-    padding: 1rem;
-
-`;
-
-
-const SettingsFormStyle = styled.div`
-/* position: absolute; */
-/* top: 10%;
-right: 5%;
-
-z-index: 1; */
-
-
-
-
-label {
-    
-}
-
-input {
-    /* float: right; */
-    width: 50%;
-    height: 2em;
-    /* padding-left: 1em;
-    padding-right: 1em; */
-    font-size: 15px;
-    border-radius: 15px;
-}
-input:focus {
-    box-shadow: 0px 0px 1px 2px #17B978;
-    outline: none;
-}
-
-`;
-
 
 
 export default CurrentWeatherCardBack;
