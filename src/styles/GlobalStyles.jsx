0ac344cff5d793f34b2bbb1003debbdb46dcafd4
cccaaannn <can.kurt.aa@gmail.com>
2021-07-31 00:32:20 +0300

    header styles added

--- src/styles/GlobalStyles.jsx
+++ src/styles/GlobalStyles.jsx
@@ -2,7 +2,7 @@ import { createGlobalStyle } from "styled-components";
 
 const GlobalStyles = createGlobalStyle`
     :root{
-        --primary-color:#007bff;
+        /* --primary-color:#007bff;
         --secondary-color:#6c757d;
         --background-dark-color:#10121A;
         --background-dark-grey:#191d2b;
@@ -15,7 +15,7 @@ const GlobalStyles = createGlobalStyle`
         --font-dark-color-2:#151515;
         --sidebar-dark-color:#191d2b;
         --scrollbar-bg-color: #6b6b6b;
-        --scrollbar-track-color: #383838; 
+        --scrollbar-track-color: #383838;  */
 
         --card-color1: #086972;
         --card-color2: #17B978;
@@ -28,28 +28,32 @@ const GlobalStyles = createGlobalStyle`
         --button-color1: #3c3c3c;
         --button-color2: #17B978;
 
-
+        --background-color: #071A52;
 
     }
 
     *{
-        margin:0; 
-        padding:0;
-        box-sizing:border-box;
-        list-style:none;
-        text-decoration:none;
-        font-size:1.2rem;
+        margin: 0; 
+        padding: 0;
+        box-sizing: border-box;
+        list-style: none;
+        text-decoration: none;
+        font-size: 1.2rem;
     }
 
     body{
         font-family: Arial, Helvetica, sans-serif;
         background-color: #071A52 !important;
-        color: #ffff;
-        color: var(--text-primary) !important;
+        color: var(--text-primary);
     }
 
-
-
+    .header{
+        header{
+            font-family: "Handlee";
+        }
+        color: white;
+        background-color: var(--background-color);
+    }
 
     .card{
         border-radius: 15px !important;
@@ -71,9 +75,6 @@ const GlobalStyles = createGlobalStyle`
     .text-secondary{
         color: var(--text-secondary) !important;
     }
-
-    .muz{
-    transform: rotate='90deg'}
 
 `;
 
