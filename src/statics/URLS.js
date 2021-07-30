2c78f0d79efe50e4d5b7807d63433df644550b9a
cccaaannn <can.kurt.aa@gmail.com>
2021-07-30 20:46:36 +0300

    project URL added

--- src/statics/URLS.js
+++ src/statics/URLS.js
@@ -4,4 +4,6 @@ const forecastAPICallBaseURL = "https://api.openweathermap.org/data/2.5/forecast
 const iconBaseURL = "http://openweathermap.org/img/wn/";
 const placeHolderImageURL = "https://github.com/github.png";
 
-export { weatherAPICallBaseURL, forecastAPICallBaseURL, iconBaseURL, placeHolderImageURL };
+const projectURL = "https://github.com/cccaaannn/WTForecast";
+
+export { weatherAPICallBaseURL, forecastAPICallBaseURL, iconBaseURL, placeHolderImageURL, projectURL };
