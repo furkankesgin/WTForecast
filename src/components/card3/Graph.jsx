e61f04be791a0fb2af25565e7dc4d3679ba8a589
cccaaannn <can.kurt.aa@gmail.com>
2021-08-05 00:19:49 +0300

    graph updated

--- src/components/card3/Graph.jsx
+++ src/components/card3/Graph.jsx
@@ -1,111 +1,152 @@
-import { Bar} from 'react-chartjs-2';
-import {splitByHour} from "../../helpers/splitByHour";
+import { Line } from 'react-chartjs-2';
+import { splitByHour } from "../../helpers/splitByHour";
 import { weekDay } from "../../helpers/dateTime";
-import {convertTemp} from "../../helpers/convertUnits"
+import { convertTemp } from "../../helpers/convertUnits"
 import { useEffect, useState } from 'react'
-
-
-
+import UNITS from "../../statics/UNITS";
 
 const Graph = (props) => {
-  const buildGraphDataset=() =>{
-    let days = [], temp=[], humidity=[];
-    {props.allList.list ? 
-      props.allList.list.map((weather)=> splitByHour(weather,"12") ? 
-      days.push(weekDay(new Date(weather.dt * 1000).getDay()))+
-      temp.push(convertTemp({temp:weather.main.temp, unit:props.unit}))+ 
-      humidity.push(weather.main.humidity): <div></div>)
-      : <div></div>};
-    return {days: days, temp:temp, humidity:humidity}
-  }
+	const buildGraphDataset = () => {
+		let days = [], temp = [], humidity = [];
 
-  useEffect(() => {
-   const {days, temp, humidity} = buildGraphDataset()
-    setDays(days);
-    setTemperatureDataset(temp);
-    setHumidityDataset(humidity);
-  }, [props.allList, props.unit]);
+		if(props.allList.list){
+			props.allList.list.map((weather) => {		
+				if(splitByHour(weather, "12")){
+					days.push(weekDay(new Date(weather.dt * 1000).getDay()));
+					temp.push(convertTemp({ temp: weather.main.temp, unit: props.unit }));
+					humidity.push(weather.main.humidity);
+				}
+			})
+		}
 
-  const [days, setDays] = useState('');
-  const [temperatureDataset, setTemperatureDataset] = useState('');
-  const [humidityDataset, setHumidityDataset] = useState('');
+		return { days: days, temp: temp, humidity: humidity }
+	}
 
-  
-  const [tempInvisibility, setTempVisibility] = useState(false);
-  const [humidityInvisibility, setHumidityVisibility] = useState(true);
-  const [bbInvisibility, setbbVisibility] = useState(true);
+	useEffect(() => {
+		const { days, temp, humidity } = buildGraphDataset();
+		setDays(days);
+		setTemperatureDataset(temp);
+		setHumidityDataset(humidity);
+	}, [props.allList, props.unit]);
+
+	const [days, setDays] = useState('');
+	const [temperatureDataset, setTemperatureDataset] = useState('');
+	const [humidityDataset, setHumidityDataset] = useState('');
 
 
-  // const [humidityDataset2, setHumidityDataset2] = useState('');
-const a = [setTempVisibility, setHumidityVisibility, setbbVisibility];
+	const [tempInvisibility, setTempVisibility] = useState(false);
+	const [humidityInvisibility, setHumidityVisibility] = useState(true);
+	const [bbInvisibility, setbbVisibility] = useState(true);
 
-  const data = {
-    labels: days,
-    datasets: [
-      {
-        
-        type: 'line',
-        label: 'Temp',
-        hidden:tempInvisibility,
-        backgroundColor: 'rgb(75, 192, 192)',
-        // data: temp.slice(0,5),
-        data: temperatureDataset,
-        fill:true
-      },
+	const visibilityFunctions = [setTempVisibility, setHumidityVisibility, setbbVisibility];
 
-      {
-        type: 'bar',
-        label: 'Humidity %', 
-        hidden:humidityInvisibility,
-        backgroundColor: 'blue',
-        data: humidityDataset,
-        borderColor: 'white',
-        borderWidth: 2,
-        fill:true,
-      },
-      {
-        type: 'bar',
-        label: 'bb %', 
-        hidden:bbInvisibility,
-        backgroundColor: 'blue',
-        data: humidityDataset,
-        borderColor: 'white',
-        borderWidth: 2,
-        fill:true,
-      }
-      
-    ],
-  };
-  
-  const options = {
-    indexAxis: 'x',
-    elements: {
-      bar: {
-        borderWidth: 2,
-      },
-    
-    },
-    
-    responsive: true,
-    plugins: {
-      legend: {
-        position: 'top',
-        'onClick' : function (evt, item) {
-          for (let index = 0; index < data.datasets.length; index++) {
-            a[index](true);
-          }
-        a[item.datasetIndex](false);
-      },
-      },
-      title: {
-        display: false,
-        text: 'Chart.js Horizontal Bar Chart',
-      },
-    },
-  };
-  return (
-    <Bar data={data} options={options}/>
-  );
+
+	const data = {
+		labels: days,
+		datasets: [
+			{
+				type: 'line',
+				label: 'Temperature' + UNITS[props.unit].temp,
+				hidden: tempInvisibility,
+				backgroundColor: '#17B978',
+				borderColor: 'white',
+				data: temperatureDataset,
+				fill: true
+			},
+
+			{
+				type: 'line',
+				label: 'Humidity' + UNITS[props.unit].humidity,
+				hidden: humidityInvisibility,
+				backgroundColor: '#17B978',
+				borderColor: 'white',				
+				data: humidityDataset,
+				borderWidth: 2,
+				fill: true,
+			},
+
+			{
+				type: 'line',
+				label: 'bb' + UNITS[props.unit].humidity,
+				hidden: bbInvisibility,
+				backgroundColor: '#17B978',
+				borderColor: 'white',				
+				data: humidityDataset,
+				borderWidth: 2,
+				fill: true,
+			}
+
+		],
+	};
+
+
+	const options = {
+		// change legend color example  https://stackoverflow.com/questions/37292423/chart-js-label-color
+
+		indexAxis: 'x',
+		responsive: true,
+
+		// elements: {
+		// 	bar: {
+		// 		borderWidth: 2,
+		// 	},
+		// },
+
+		plugins: {
+			legend: {
+				position: 'top',
+				onClick: function (evt, item) {
+					for (let index = 0; index < data.datasets.length; index++) {
+						visibilityFunctions[index](true);
+					}
+					visibilityFunctions[item.datasetIndex](false);
+				},
+
+				labels: {
+					color: "#17B978",
+					font: {
+						size: 20
+					}
+				}
+			},
+
+			// title: {
+			// 	display: true,
+			// 	text: 'Chart.js Horizontal Bar Chart',
+			// },
+		},
+
+		scales: {
+
+			y: {
+				ticks: {
+					stepSize: 3,					
+					color: "#17B978",
+					// beginAtZero: true,
+					font: {
+						size: 20
+					}
+				}
+			},
+
+			x: {
+				ticks: {
+					stepSize: 1,
+					color: "#17B978",
+					// beginAtZero: true,						
+					font: {
+						size: 20
+					}
+				}
+			}
+
+		},
+
+	};
+
+	return (
+		<Line data={data} options={options} />
+	);
 }
 
 export default Graph;
