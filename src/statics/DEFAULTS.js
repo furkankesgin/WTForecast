a82b4d612bfe6d4f50022bc458b63d3eb04bd639
cccaaannn <can.kurt.aa@gmail.com>
2021-08-10 02:12:56 +0300

    DEFAULTS added

---
+++ src/statics/DEFAULTS.js
@@ -0,0 +1,19 @@
+const DEFAULTS = {
+    "customUIElements":
+    {
+        "pressure": true,
+        "humidity": true,
+        "wind": true,
+        "cloudiness": true,
+        "minmax": true,
+        "feels": true,
+        "lonlat": true,
+        "sun": true
+    },
+
+    "unit": "metric",
+    "city": "istanbul",
+    "theme": "light"
+}
+
+export default DEFAULTS;
