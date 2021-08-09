faf8d33c73efda9340b4c69f5402379af1d495a0
cccaaannn <can.kurt.aa@gmail.com>
2021-08-10 02:12:14 +0300

    COLORS changed to THEMES

---
+++ src/statics/THEMES.jsx
@@ -0,0 +1,24 @@
+const THEMES = {
+
+    dark: {
+        background_color: "#222831",        
+        color_primary: "#393E46",
+        color_secondary: "#017f85",
+
+        text_primary: "#eee",
+        text_secondary: "#919191",
+        text_important: "#00bdb6"
+    },
+
+    light: {
+        background_color: "#bef2ffb9",        
+        color_primary: "#00B7C2",
+        color_secondary: "#128494",
+
+        text_primary: "#3c3c3c",
+        text_secondary: "#ffffff",
+        text_important: "#43ff39a2"
+    }
+
+}
+export default THEMES;
