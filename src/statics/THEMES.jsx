d73b2d043aa8b82856c00f2d0387cacfd6d88c0f
cccaaannn <can.kurt.aa@gmail.com>
2021-08-12 00:21:10 +0300

    themes updated

--- src/statics/THEMES.jsx
+++ src/statics/THEMES.jsx
@@ -1,23 +1,37 @@
 const THEMES = {
 
     dark: {
+        // general
         background_color: "#222831",        
         color_primary: "#393E46",
         color_secondary: "#017f85",
 
+        // text
         text_primary: "#eee",
         text_secondary: "#919191",
-        text_important: "#00bdb6"
+        text_important: "#00bdb6",
+        
+        // form
+        form_placeholder: "#919191",
+        valid_input: "#08ceff",
+        invalid_input: "#ff006acc"
     },
 
     light: {
+        // general
         background_color: "#bef2ffb9",        
         color_primary: "#00B7C2",
         color_secondary: "#128494",
 
+        // text
         text_primary: "#3c3c3c",
         text_secondary: "#ffffff",
-        text_important: "#43ff39a2"
+        text_important: "#43ff39a2",
+
+        // form
+        form_placeholder: "#919191",
+        valid_input: "#43ff39a2",
+        invalid_input: "#ff0077ce"
     }
 
 }
