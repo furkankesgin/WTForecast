b83da088e89f58cfeadbe14e487632cf1f419c27
cccaaannn <can.kurt.aa@gmail.com>
2021-08-30 16:14:07 +0300

    more default values added

--- src/statics/DEFAULTS.js
+++ src/statics/DEFAULTS.js
@@ -1,19 +1,38 @@
 const DEFAULTS = {
-    "customUIElements": {
-        "pressure": true,
-        "humidity": true,
-        "wind": true,
-        "cloudiness": true,
-        "minmax": true,
-        "feels": true,
-        "lonlat": true,
-        "sun": true
+
+    storageNames:{
+        // items will be stored with this names on the storage 
+        applicationName: "WTForecast",
+
+        customUIElements: "customUIElements",
+        unit: "unit",
+        city: "city",
+        theme: "theme",
+        useSystemTheme: "useSystemTheme"
     },
 
-    "unit": "metric",
-    "city": "istanbul",
-    "theme": "light",
-    "useSystemTheme": true
+    storageDefaults: {
+        // default values for storage items
+        "customUIElements": {
+            "pressure": true,
+            "humidity": true,
+            "wind": true,
+            "cloudiness": true,
+            "minmax": true,
+            "feels": true,
+            "lonlat": true,
+            "sun": true
+        },
+
+        "unit": "metric",
+        "city": "istanbul",
+        "theme": "light",
+        "useSystemTheme": true
+    },
+
+    // other defaults
+    cityFormDebounceTime: 1000,
+    weatherFetchInterval: 1000 * 60 * 60 // 1 hour
 }
 
 export default DEFAULTS;
