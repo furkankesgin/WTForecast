2b3823000557618ebb198d5675ea5def02006eb8
cccaaannn <can.kurt.aa@gmail.com>
2021-08-31 01:20:32 +0300

    useStateWithAutoSave updated to use new storage class

--- src/hooks/useStateWithAutoSave.jsx
+++ src/hooks/useStateWithAutoSave.jsx
@@ -1,13 +1,23 @@
+// react
 import { useState, useEffect } from 'react';
 
-const useStateWithAutoSave = ({storage, storageName}) => {
-    // custom hook for creating a stateful element and auto saving it into local storage
-    // uses getItemFromStorageWithDefaultFallback from the storage class for preventing undefined values
+// statics
+import DEFAULTS from "../statics/DEFAULTS";
 
-    const [item, setItem] = useState(storage.getItemFromStorageWithDefaultFallback(storageName));
+// helpers
+import Storage from '../helpers/Storage';
+
+const useStateWithAutoSave = (storageName) => {
+    // custom hook for creating a stateful element and auto saving it into local storage on change
+    // uses storage class
+
+    // init storage object with default values
+    const storage = new Storage(DEFAULTS.storageNames.applicationName, DEFAULTS.storageDefaults);
+
+    const [item, setItem] = useState(storage.getItemFromStorageWithDefaultFallback(storageName)); // getItemFromStorageWithDefaultFallback returns something all the time
 
     useEffect(() => {
-		storage.setToStorage({ [storageName]: item });
+		storage.addItemToStorage({ [storageName]: item });
 	}, [item])
 
     return [item, setItem];
