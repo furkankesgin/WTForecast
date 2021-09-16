991710111c79bc6ee26f54caa187fc992a97df2b
cccaaannn <can.kurt.aa@gmail.com>
2021-09-16 21:30:15 +0300

    image url changed to https

--- src/statics/URLS.js
+++ src/statics/URLS.js
@@ -1,7 +1,7 @@
 const weatherAPICallBaseURL = "https://api.openweathermap.org/data/2.5/weather";
 const forecastAPICallBaseURL = "https://api.openweathermap.org/data/2.5/forecast";
 
-const iconBaseURL = "http://openweathermap.org/img/wn/";
+const iconBaseURL = "https://openweathermap.org/img/wn/";
 const placeHolderImageURL = "https://github.com/github.png";
 
 const projectURL = "https://github.com/cccaaannn/WTForecast";
