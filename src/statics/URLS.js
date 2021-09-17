447eb8b8b2d4aa9e1bc20a191ffc19a0e322f02f
cccaaannn <can.kurt.aa@gmail.com>
2021-09-17 04:12:06 +0300

    placeholder image changed

--- src/statics/URLS.js
+++ src/statics/URLS.js
@@ -2,7 +2,7 @@ const weatherAPICallBaseURL = "https://api.openweathermap.org/data/2.5/weather";
 const forecastAPICallBaseURL = "https://api.openweathermap.org/data/2.5/forecast";
 
 const iconBaseURL = "https://openweathermap.org/img/wn/";
-const placeHolderImageURL = "https://github.com/github.png";
+const placeHolderImageURL = "https://raw.githubusercontent.com/cccaaannn/WTForecast/master/public/icons/icon-192x192.png";
 
 const projectURL = "https://github.com/cccaaannn/WTForecast";
 
